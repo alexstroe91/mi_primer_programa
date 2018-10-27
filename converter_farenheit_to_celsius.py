20bc7cebe60ac9aac0e0f1a1f2ceb6e0478c3bf1
alexstroe91 <alexstroe91@gmail.com>
2018-10-27 02:12:09 +0200

    Adding the farenheit to celsius converter :)

---
+++ converter_farenheit_to_celsius.py
@@ -0,0 +1,6 @@
+
+farenhei_degrees = float(input("Inserte los grados farenheit a convertir aquí: "))
+
+celsius_degrees = (farenhei_degrees - 32) / 1.8
+
+print ("Los grados en celsius son {}".format(celsius_degrees))
